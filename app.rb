113309ab5a543c3a1f199f238ed545ed57ef42e6
codetweak <codetweak@live.com>
2014-11-11 15:51:45 -0800

    Update app.rb

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 configure do
-  puts "Hello world ... @@@"
+  puts "Hello world666 ... @@@"
   puts "Running app file"
   puts "Create database..."
   %x"rake db:create"
